4248fb1ca948139d2956ebe44bb77ebb6afa5e4e
[Sukhwinder Singh] <[sukhisb31@gmail.coml]>
2025-04-18 22:12:48 +0530

    till home

--- frontend/src/layout/SideDrawer.jsx
+++ frontend/src/layout/SideDrawer.jsx
@@ -1,10 +1,23 @@
-import React from 'react'
+import React, { useState } from 'react';
+import { RiAuctionFill } from "react-icons/ri";
+import { MdLeaderboard, MdDashboard } from "react-icons/md";
+import { SiGooglesearchconsole } from "react-icons/si";
+import { BsFillInfoSquareFill } from "react-icons/bs";
+import { FaFacebook } from "react-icons/fa";
+import { RiInstagramFill } from "react-icons/ri";
+import { GiHamburgerMenu } from "react-icons/gi";
+import { IoMdCloseCircleOutline, IoIosCreate } from "react-icons/io";
+import { FaUserCircle } from "react-icons/fa";
+import { FaFileInvoiceDollar } from "react-icons/fa6";
+import { FaEye } from "react-icons/fa";
 
 const SideDrawer = () => {
+    const [ show, setShow ] = useState(false);
+
     return (
-        <div>
+        <>
             
-        </div>
+        </>
     )
 }
 
